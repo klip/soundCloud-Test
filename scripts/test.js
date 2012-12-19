2b2c47e0eb659d13586b7d18edc0d0fef7143ff1
Igor Raihlin <design.klipart@gmail.com>
2012-12-19 19:12:27 +0200

    bookmarklet

--- scripts/test.js
+++ scripts/test.js
@@ -298,7 +298,7 @@ var fUtils = {
 
             $(this).off('DOMSubtreeModified');
 
-            var w_code = encodeURIComponent($(fUtils.settings.selectors.sc_sh_cont, d_box).val());
+            var w_code = $(fUtils.settings.selectors.sc_sh_cont, d_box).val();
 
             $("body").append("<iframe width='280' height='50' frameBorder='0'  id='"+fUtils.settings.selectors.remoteSCIframe.replace("#","")+"' style='display:none;position:absolute;z-index:1000;right:100px;top:10px;width:280px;border-radius:6px;' src='http://klip.grm.im/git/SCtest.git/soundCloud-Test/addFromSC.html#"+w_code+"'></iframe>");
             var _form = $('#sc_remote_add_to_pl');
