dc9cf6f194b555bf9ad27b1681a5fb94eaf5e17d
Igor Raihlin <design.klipart@gmail.com>
2012-12-20 10:10:17 +0200

    bookmarklet

--- scripts/test.js
+++ scripts/test.js
@@ -298,7 +298,7 @@ var fUtils = {
             var _d_box =$(this);
             _d_box.off('DOMSubtreeModified');
             var wg = $(fUtils.settings.selectors.sc_sh_cont, _d_box);
-            if(wg.length<1 || window.p_cd==true) {
+            if(wg.length<1) {
                 shr_btn.click();
             }else{
                 var w_code = $(fUtils.settings.selectors.sc_sh_cont, _d_box).val();
