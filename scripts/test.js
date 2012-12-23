ce11e21945f5825f5763aae3a1aa4a7c2da0a31b
Igor Raihlin <design.klipart@gmail.com>
2012-12-23 11:14:36 +0200

    oEmbed fix

--- scripts/test.js
+++ scripts/test.js
@@ -264,6 +264,7 @@ var fUtils = {
         });*/
 
         SC.get('/resolve', {url:track_url}, function(resolve){
+            console.log(resolve);
             $track= '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url='+resolve.location.replace('.json','')+'"></iframe>';
         });
 
